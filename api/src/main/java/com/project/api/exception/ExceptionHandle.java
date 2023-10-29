bb209acf613e8670870356a4bfd3083ec2997d9d
Geraldo Filho <geraldofilho1069@gmail.com>
2023-10-29 16:15:11 -0300

    feat: Novas exceptions inseridas

--- api/src/main/java/com/project/api/exception/ExceptionHandle.java
+++ api/src/main/java/com/project/api/exception/ExceptionHandle.java
@@ -1,13 +1,15 @@
 package com.project.api.exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.FormatterClosedException;
 
 @RestControllerAdvice
 public class ExceptionHandle {
@@ -22,5 +24,19 @@ public class ExceptionHandle {
         var erros = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(ExceptionHandlerDto::new).toList());
     }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity Error409(){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito");
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
+    public ResponseEntity Error401(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Ação Não Autorizada");
+    }
+    @ExceptionHandler(FormatterClosedException.class)
+    public ResponseEntity Erro403(){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Sem direito de acesso ao conteúdo solicitado");
+    }
+
 
 }
